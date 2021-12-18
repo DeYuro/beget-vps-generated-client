fd9dd630b480b7e46328100fab293f72c26414ff
Denis Shashkov <dshashkov@beget.ru>
2021-12-18 21:57:09 +0300

    assets

--- assets/AppAsset.php
+++ assets/AppAsset.php
@@ -20,12 +20,19 @@ class AppAsset extends AssetBundle
     public $basePath = '@webroot';
     public $baseUrl = '@web';
     public $css = [
-        'css/site.css',
+//        'css/site.css',
+        'https://cdn.jsdelivr.net/npm/bootstrap-icons@1.5.0/font/bootstrap-icons.css',
+//        'https://fonts.googleapis.com/css?family=Lato:300,400,700,300italic,400italic,700italic',
+        'css/styles.css',
     ];
     public $js = [
+        'https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js',
+        'js/scripts.js',
+        'https://cdn.startbootstrap.com/sb-forms-latest.js'
     ];
     public $depends = [
-        'yii\web\YiiAsset',
-        'yii\bootstrap4\BootstrapAsset',
+//        FontAwesomeAsset::class,
+//        'yii\web\YiiAsset',
+//        'yii\bootstrap4\BootstrapAsset',
     ];
 }
