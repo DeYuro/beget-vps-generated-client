2ff3afdd21fcd5f57a22f8de97d885255bc86c33
Denis Shashkov <dshashkov@beget.ru>
2021-12-18 22:43:39 +0300

    faq

---
+++ views/site/faq.php
@@ -0,0 +1,102 @@
+<?php
+/* @var $this yii\web\View */
+
+
+$this->title = 'FAQ';
+$this->params['breadcrumbs'][] = $this->title;
+?>
+
+<!--Section: FAQ-->
+<div class="container">
+
+    <div class="row">
+        <div class="col-lg-6 col-md-12 mb-4">
+
+            <!--Questions -->
+            <section>
+
+                <!--Question -->
+                <div>
+                    <p class="mb-1">
+                        <strong>Standard length question?</strong>
+                    </p>
+                    <p class="mb-1">
+                        <u>Higlighted short answer.</u> And some kind of detailed list.
+                    </p>
+                    <ul>
+                        <li>list item 1</li>
+                        <li>list item 2</li>
+                        <li>list item 3</li>
+                    </ul>
+                </div>
+
+                <!--Question -->
+                <div>
+                    <p class="mb-1">
+                        <strong>Short question?</strong>
+                    </p>
+                    <p class="mb-1">
+                        <u>Higlighted short answer.</u>
+                    </p>
+                    <p class="mb-1">
+                        Second part of the answer with more details.
+                    </p>
+                    <p>
+                        Final part of the answer full of detais and Lorem ipsum dolor sit amet consectetur
+                        adipisicing elit.
+                    </p>
+                </div>
+                <!--Question -->
+                <div>
+                    <p class="mb-1">
+                        <strong>The longest question in this faq. Made up of two sentences in order to span to the
+                            next line?</strong>
+                    </p>
+                    <p class="mb-1">
+                        <u>Higlighted short answer.</u>
+                    </p>
+                    <p class="mb-1">
+                        Second part of the answer with more details.
+                    </p>
+                    <p>
+                        Final part of the answer full of detais and Lorem ipsum dolor sit amet consectetur
+                        adipisicing elit.
+                    </p>
+                </div>
+
+                <!--Question -->
+                <div>
+                    <p class="mb-1">
+                        <strong>Slightly longer question?</strong>
+                    </p>
+                    <p class="mb-1">
+                        <u>Higlighted short answer.</u>
+                    </p>
+                    <p class="mb-1">
+                        Second part of the answer with more details.
+                    </p>
+                    <p>
+                        Final part of the answer full of detais and Lorem ipsum dolor sit amet consectetur
+                        adipisicing elit.
+                    </p>
+                </div>
+
+            </section>
+            <!--/Questions visible to all-->
+
+
+        </div>
+
+        <div class="col-lg-6 col-md-12 text-center">
+
+
+            <!--Section: Contact Form-->
+            <section>
+
+            </section>
+            <!--/Section: Newsletter-->
+        </div>
+    </div>
+</div>
+
+<!--Section: FAQ-->
